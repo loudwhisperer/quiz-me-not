49baf906bd8dc4cfb31768b8ddb8a06c6f1155e7
loudwhisperer <driftaway44@gmail.com>
2022-10-30 22:11:37 -0600

    first try at creating a function to add a page of questions dynamically in quiz-function.js

---
+++ assets/js/quiz-function.js
@@ -0,0 +1,9 @@
+// TODO: add function to create pages dynamically
+const createPageOne = function(){
+const pone = document.createElement('div');
+pone.setAttribute('id', 'pageone')
+// const optone = document.pone.appendChild(document.createElement('input'))
+// optone.setAttribute('type', 'radio')
+// document.poneoptone.appendChild(document.createElement('label', labelfor='poneoptone'))
+}
+window.onload = createPageOne();
