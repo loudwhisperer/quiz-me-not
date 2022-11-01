2fa300d5e92b7a2618001468dfde20969d3cb9c3
loudwhisperer <driftaway44@gmail.com>
2022-10-31 22:19:10 -0600

    added a variable to store questions,options and correct answers then added variables to store the score the current number of answer options and an array to store the chosen answer options to local stoarge for later as well as started a function to create questions dynamically

--- assets/js/quiz-function.js
+++ assets/js/quiz-function.js
@@ -1,9 +1,36 @@
 // TODO: add function to create pages dynamically
-const createPageOne = function(){
-const pone = document.createElement('div');
-pone.setAttribute('id', 'pageone')
-// const optone = document.pone.appendChild(document.createElement('input'))
-// optone.setAttribute('type', 'radio')
-// document.poneoptone.appendChild(document.createElement('label', labelfor='poneoptone'))
+const questions = [
+  {
+    question: "Q1: Do we have a leadership?",
+    options: ["yes", "no"],
+    corectAnswer: 0,
+  },
+  {
+    question: "Q2: What designation does a field agent have?",
+    options: ["seeker", "informant", "networker"],
+    correctAnswer: 0,
+  },
+  {
+    question: "Q3: What designation does a information gatherer have?",
+    options: ["seeker", "informant", "networker"],
+    correctAnswer: 1,
+  },
+  {
+    question: "Q4: What designation does someone who forms connections for the project have?",
+    options: ["seeker", "informant", "networker"],
+    correctAnswer: 2,
+  },
+];
+let score = 0;
+let currentInteger = 0;
+let chosenOptions= [];
+
+const createQuestion = () =>{
+    for (let i=0; i<questions[this.currentNumber].options.length;i++){
+        document.forms.radios.elements.option[i].checked=false;
+    }
+    let question=document.getElementById("question");
+    question.innerHTML
 }
-window.onload = createPageOne();
+
+
